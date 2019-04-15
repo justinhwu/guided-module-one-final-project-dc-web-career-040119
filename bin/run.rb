8065d5903f591c9f7f2670b816f3b52b58456d74
Justin <justinhwu95@gmail.com>
2019-04-15 17:33:33 -0400

    Added CLI class

--- bin/run.rb
+++ bin/run.rb
@@ -1,2 +1,3 @@
-require_relative 'config/environment'
-
+require_relative '../config/environment'
+new_cli = CommandLineInterface.new
+new_cli.run
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,13 @@ Cuisine.destroy_all
 Restaurant.destroy_all
 
 american = Cuisine.find_or_create_by(name: "American")
+chinese = Cuisine.find_or_create_by(name: "Chinese")
+mexican = Cuisine.find_or_create_by(name: "Mexican")
 
 gallery_pl = Neighborhood.find_or_create_by(name: "Gallery Place")
+dupont = Neighborhood.find_or_create_by(name: "Dupont Circle")
+metro_center = Neighborhood.find_or_create_by(name: "Metro Center")
 
 jacks = Restaurant.find_or_create_by(name: "Jack's Fresh", price: "$", family_friendly: true, cuisine_id: american.id, neighborhood_id: gallery_pl.id)
+chipotle = Restaurant.find_or_create_by(name: "Chipotle", price: "$", family_friendly: false, cuisine_id: mexican.id, neighborhood_id: dupont.id)
+pf_chang = Restaurant.find_or_create_by(name: "PF Chang's", price: "$$", family_friendly: true, cuisine_id: chinese.id, neighborhood_id: metro_center.id)
